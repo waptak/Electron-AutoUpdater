19c480e37c242b0bb0d3619d353418b698fb1139
waptak <waptak@gmail.com>
2020-08-06 14:46:30 +0800

    远程更新已好

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow ,autoUpdater , dialog} = require('electron')
+const {app, BrowserWindow  , dialog} = require('electron')
 const path = require('path')
-const log = require('electron-log');
-log.transports.file.file = path.join(__dirname, 'log.log');
+// const log = require('electron-log');
+const { autoUpdater } = require('electron-updater');
+// log.transports.file.file = path.join(__dirname, 'log.log');
 
+// autoUpdater.logger = log;
+// autoUpdater.logger.transports.file.level = 'info';
+let mainWindow
+function sendStatusToWindow(text) {
 
-
+  mainWindow.webContents.send('message', text);
+}
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration:true,
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
+  
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
+  
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
+  
 }
 
 // This method will be called when Electron has finished
@@ -28,16 +36,19 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
- 
+  
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-
-  initUpdates()
+  setTimeout(() => {
+    autoUpdater.checkForUpdatesAndNotify();
+  }, 2000);
+ 
+  
 }).catch(error=>{
-  log.info('whenReady', JSON.stringify(error))
+  // log.info('whenReady', JSON.stringify(error))
   console.error(error)
 });
 
@@ -51,114 +62,22 @@ app.on('window-all-closed', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (ev, info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (ev, info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (ev, err) => {
+  sendStatusToWindow('Error in auto-updater.');
+})
+autoUpdater.on('download-progress', (ev, progressObj) => {
+  sendStatusToWindow('Download progress...'+ JSON.stringify(progressObj));
+})
+autoUpdater.on('update-downloaded', (ev, info) => {
+  sendStatusToWindow('Update downloaded; will install in 5 seconds');
+});
 
-
-function initUpdates(){
-  if (require('electron-squirrel-startup')) return;
-  autoUpdater.setFeedURL('http://192.168.30.111:7430/upload/electron');
-
-  autoUpdater.on('checking-for-update', () => {
-    log.info('checking-for-update')
-  });
-
-  autoUpdater.on('update-available', () => {
-    log.info('update-available')
-  });
-
-  autoUpdater.on('update-not-available', () => {
-    log.info('update-not-available')
-  });
-
-  autoUpdater.on('update-downloaded', () => {
-    log.info('update-downloaded')
-    dialog.showMessageBox({type:'warning' ,title:'软件更新' ,message:'新版本已下载，是否确定更新？' , buttons:['取消','确定']})
-    .then(idx=>{
-      if(idx.response == 1){
-        autoUpdater.quitAndInstall();
-      }
-    })
-    
-  });
-
-    let squirrelEvent = process.argv[1];
-    log.info('squirrelEvent',squirrelEvent)
-    if (squirrelEvent !== '--squirrel-firstrun'){
-      autoUpdater.checkForUpdates()
-      log.info('not-squirrel-firstrun')
-    }  
-    else{
-      log.info('squirrel-firstrun')
-    }
-}
-
-
-
-
-
-// // this should be placed at top of main.js to handle setup events quickly
-// if (handleSquirrelEvent()) {
-//   // squirrel event handled and app will exit in 1000ms, so don't do anything else
-//   return;
-// }
-
-// function handleSquirrelEvent() {
-//   if (process.argv.length === 1) {
-//     return false;
-//   }
-
-//   const ChildProcess = require('child_process');
-
-
-//   const appFolder = path.resolve(process.execPath, '..');
-//   const rootAtomFolder = path.resolve(appFolder, '..');
-//   const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-//   const exeName = path.basename(process.execPath);
-
-//   const spawn = function(command, args) {
-//     let spawnedProcess, error;
-
-//     try {
-//       spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
-//     } catch (error) {}
-
-//     return spawnedProcess;
-//   };
-
-//   const spawnUpdate = function(args) {
-//     return spawn(updateDotExe, args);
-//   };
-
-//   const squirrelEvent = process.argv[1];
-//   switch (squirrelEvent) {
-//     case '--squirrel-install':
-//     case '--squirrel-updated':
-//       // Optionally do things such as:
-//       // - Add your .exe to the PATH
-//       // - Write to the registry for things like file associations and
-//       //   explorer context menus
-
-//       // Install desktop and start menu shortcuts
-//       spawnUpdate(['--createShortcut', exeName]);
-
-//       setTimeout(app.quit, 1000);
-//       return true;
-
-//     case '--squirrel-uninstall':
-//       // Undo anything you did in the --squirrel-install and
-//       // --squirrel-updated handlers
-
-//       // Remove desktop and start menu shortcuts
-//       spawnUpdate(['--removeShortcut', exeName]);
-
-//       setTimeout(app.quit, 1000);
-//       return true;
-
-//     case '--squirrel-obsolete':
-//       // This is called on the outgoing version of your app before
-//       // we update to the new version - it's the opposite of
-//       // --squirrel-updated
-
-//       app.quit();
-//       return true;
-//   }
-// };
--- renderer.js
+++ renderer.js
@@ -4,3 +4,8 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const { shell, ipcRenderer, Menu } = require('electron');
+
+ipcRenderer.on('message', (event, message) => {
+    console.log(message)
+})
